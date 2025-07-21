a9a8c1e02adca60ce4339ad00701e90ca9da3432
Golovko, Yaroslav <golovko085@gmail.com>
2025-07-21 18:05:00 +0700

    basic project structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kasariks/authorization
+
+go 1.24.0
---
+++ internal/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/kasariks/authorization/internal/handlers"
+)
+
+type routerData struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+func CreateServer(logger *log.Logger) (*routerData, error) {
+	// Create db here
+
+	registerHandlers()
+
+	router := newRouterData(logger)
+	return router, nil
+}
+
+func registerHandlers() {
+	http.HandleFunc("/", handlers.MainHandler)
+}
+
+func newRouterData(logger *log.Logger) *routerData {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		IdleTimeout:  time.Second * 5,
+	}
+	router := &routerData{
+		Logger: logger,
+		Server: server,
+	}
+	return router
+}
