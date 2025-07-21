35ebb10432e295a60d19775a59f07889cd5e125c
Golovko, Yaroslav <golovko085@gmail.com>
2025-07-21 18:04:09 +0700

    basic project structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/kasariks/authorization/internal/server"
+)
+
+func main() {
+	logger := &log.Logger{}
+	router, err := server.CreateServer(logger)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = http.ListenAndServe(router.Server.Addr, router.Server.Handler); err != nil {
+		log.Fatal(err)
+	}
 }
