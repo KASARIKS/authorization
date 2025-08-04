ed3582264c31d104dce771c6481cf1a578598e09
Golovko, Yaroslav <golovko085@gmail.com>
2025-08-05 00:37:40 +0700

    save jwt, but wrong jwt-type

---
+++ internal/handlers/authorization.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/kasariks/authorization/internal/db/dbuser"
+)
+
+const jwtKey = "oepuqtpowejfvgc;lvmjn290331pq;woerwqje[p]"
+const jwtExistingTimeMinutes = 2
+
+func AuthorizationPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./tmp/html/authorization.html")
+}
+
+// Todo - fix jwt type
+func Authorization(w http.ResponseWriter, r *http.Request) {
+	err := handlersDb.CheckUserPassword(dbuser.NewDbUser(
+		r.PostFormValue("Nickname"),
+		r.PostFormValue("Password")))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	claims := jwt.MapClaims{
+		"Nickname": r.PostFormValue("Nickname"),
+		"exp":      time.Now().Add(time.Minute * jwtExistingTimeMinutes).Unix(),
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := jwtToken.SignedString(jwtKey)
+	if err != nil {
+		io.WriteString(w, fmt.Sprintf("failed to sign jwt: %s\n", err))
+	}
+
+	cookie := http.Cookie{
+		Name:     "jwtToken",
+		Value:    signedToken,
+		Expires:  time.Now().Add(time.Minute * jwtExistingTimeMinutes),
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+		Path:     "/",
+	}
+
+	http.SetCookie(w, &cookie)
+}
